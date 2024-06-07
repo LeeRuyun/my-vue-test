d069469c9e56c6c5e2b6af09765111ff53a749a6
LeeRuyun <a0909841680@gmail.com>
2024-06-07 13:06:04 +0800

    Add files via upload

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,29 @@
+<template>
+    <form id="form" @submit.prevent="onSubmit">
+        <div>
+            <input
+                v-model="text"
+                type="text"
+                placeholder="Todo Text.."
+            />
+        </div>
+
+        <div class="mt-2">
+            <button>Add Todo</button>
+        </div>
+    </form>
+</template>
+
+<script setup>
+import {ref,defineEmits} from "vue";
+
+const text =ref("")
+const emit = defineEmits(["addTodo"])
+const onSubmit=()=>{
+    const todo={
+        id:crypto.randomUUID(),
+        text:text.value,
+        complete:false
+    }
+    emit("addTodo",todo)}
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul v-if="todos.length>0">
+        <li v-for="todo in todos" :key="todo.id" class="flex gap-3 my-2">
+            <button class="w-5" @click="deleteTodo(todo.id)">x</button>
+            <button class="w-5" @click="completeTodo(todo.id)">v</button>
+            <p :class="{'line-through' : todo.complete}">{{ todo.text }}</p>
+        </li>
+    </ul>
+    <p v-else>No todos found.</p>
+</template>
+
+<script setup>
+import {defineProps,defineEmits} from "vue"
+
+defineProps({
+    todos:{
+        type:Array
+    }
+})
+const emit = defineEmits(['deleteTodo','completeTodo'])
+
+const deleteTodo=(todoId)=>{
+    emit('deleteTodo',todoId)
+}
+const completeTodo=(todoId)=>{
+    emit('completeTodo',todoId)
+}
+</script>
